0d188eddb44927fd88c9fcf9bf3be7e7647bdfbf
CeriCam <cherycamberly2005@gmail.com>
2025-12-25 03:52:49 +0700

    update cleaning script

--- Assets/Script/PickUpItem.cs
+++ Assets/Script/PickUpItem.cs
@@ -4,28 +4,17 @@ using UnityEngine;
 
 public class PickUpItem : MonoBehaviour
 {
-<<<<<<< HEAD
     public bool hasItem = true;
-    public GameObject pressEPopup; // Tarik UI "Press E" Anda ke sini
+    public GameObject pressEPopup; 
     private bool isPlayerInRange = false;
 
     void Start()
     {
-        if (pressEPopup != null) pressEPopup.SetActive(false); // Sembunyikan saat mulai
+        if (pressEPopup != null) pressEPopup.SetActive(false);
     }
 
     void Update()
     {
-=======
-    [Header("Is this a Real Item?")]
-    public bool hasItem = true; // CHECK this box for real items. UNCHECK for empty ones.
-
-    private bool isPlayerInRange = false;
-
-    void Update()
-    {
-        // Only run if player is close AND presses E
->>>>>>> 1e736a4de4e07d4d3a738a10a0fb2da4da44e9c9
         if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
         {
             CheckTheBox();
@@ -36,25 +25,10 @@ public class PickUpItem : MonoBehaviour
     {
         if (hasItem)
         {
-<<<<<<< HEAD
             GameManager.instance.CollectItem();
         }
         
-        if (pressEPopup != null) pressEPopup.SetActive(false); // Sembunyikan setelah box hancur
-=======
-            // CASE A: It has an item
-            Debug.Log(">>> SUCCESS! You found an item!");
-            GameManager.instance.CollectItem(); // Tell the manager
-        }
-        else
-        {
-            // CASE B: It is empty
-            Debug.Log(">>> EMPTY! Nothing here.");
-        }
-
-        // FINAL STEP: Make the square disappear (Destroy it)
-        // This happens for BOTH empty and real boxes so you can't check them twice.
->>>>>>> 1e736a4de4e07d4d3a738a10a0fb2da4da44e9c9
+        if (pressEPopup != null) pressEPopup.SetActive(false);
         Destroy(gameObject);
     }
 
@@ -63,11 +37,7 @@ public class PickUpItem : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             isPlayerInRange = true;
-<<<<<<< HEAD
-            if (pressEPopup != null) pressEPopup.SetActive(true); // Munculkan "Press E"
-=======
-            Debug.Log("Press 'E' to search...");
->>>>>>> 1e736a4de4e07d4d3a738a10a0fb2da4da44e9c9
+            if (pressEPopup != null) pressEPopup.SetActive(true);
         }
     }
 
@@ -76,13 +46,7 @@ public class PickUpItem : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             isPlayerInRange = false;
-<<<<<<< HEAD
-            if (pressEPopup != null) pressEPopup.SetActive(false); // Sembunyikan "Press E"
+            if (pressEPopup != null) pressEPopup.SetActive(false);
         }
     }
 }
-=======
-        }
-    }
-}
->>>>>>> 1e736a4de4e07d4d3a738a10a0fb2da4da44e9c9
